4456353d85e8e695fef396c64c7d33834d0d3940
Frank Muthomi <frankmuthomi30@gmail.com>
2026-01-22 06:36:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4079382461-f967b",
+  "appId": "1:198268241550:web:d0782acc8f771118506bdf",
+  "apiKey": "AIzaSyAIr4ZpUBUf6XPyrSCel2qiznL_eOveGVI",
+  "authDomain": "studio-4079382461-f967b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "198268241550"
+};
