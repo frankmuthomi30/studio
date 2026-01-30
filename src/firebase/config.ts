b493518cefe25a98c6a10c5913b2b690932b78f8
Frank Muthomi <frankmuthomi30@gmail.com>
2026-01-30 17:20:48 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "studio-4079382461-f967b",
-  "appId": "1:198268241550:web:d0782acc8f771118506bdf",
-  "apiKey": "AIzaSyAIr4ZpUBUf6XPyrSCel2qiznL_eOveGVI",
-  "authDomain": "studio-4079382461-f967b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "198268241550"
+  apiKey: "AIzaSyCBkPBmKqMxoQ0JzWAxKXcrOcAZotQImpM",
+  authDomain: "fir-test-b67b7.firebaseapp.com",
+  databaseURL: "https://fir-test-b67b7-default-rtdb.firebaseio.com",
+  projectId: "fir-test-b67b7",
+  storageBucket: "fir-test-b67b7.appspot.com",
+  messagingSenderId: "903240098194",
+  appId: "1:903240098194:web:c148d08f9595d2713f214b",
+  measurementId: "G-881JKM06T2"
 };
