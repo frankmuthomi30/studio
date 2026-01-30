8b46f984aa98bb797e9923d5a0894a6161347822
Frank Muthomi <frankmuthomi30@gmail.com>
2026-01-30 16:47:15 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCBkPBmKqMxoQ0JzWAxKXcrOcAZotQImpM",
-  authDomain: "fir-test-b67b7.firebaseapp.com",
-  databaseURL: "https://fir-test-b67b7-default-rtdb.firebaseio.com",
-  projectId: "fir-test-b67b7",
-  storageBucket: "fir-test-b67b7.firebasestorage.app",
-  messagingSenderId: "903240098194",
-  appId: "1:903240098194:web:c148d08f9595d2713f214b",
-  measurementId: "G-881JKM06T2"
+  "projectId": "studio-4079382461-f967b",
+  "appId": "1:198268241550:web:d0782acc8f771118506bdf",
+  "apiKey": "AIzaSyAIr4ZpUBUf6XPyrSCel2qiznL_eOveGVI",
+  "authDomain": "studio-4079382461-f967b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "198268241550"
 };
--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -9,6 +9,8 @@ import {
   QuerySnapshot,
   CollectionReference,
 } from 'firebase/firestore';
+import { errorEmitter } from '@/firebase/error-emitter';
+import { FirestorePermissionError } from '@/firebase/errors';
 
 /** Utility type to add an 'id' field to a given type T. */
 export type WithId<T> = T & { id: string };
@@ -82,17 +84,31 @@ export function useCollection<T = any>(
         setError(null);
         setIsLoading(false);
       },
-      (err: FirestoreError) => {
-        // Just pass the raw Firestore error up
-        console.error("useCollection Firestore Error:", err);
-        setError(err);
-        setData(null);
-        setIsLoading(false);
+      (error: FirestoreError) => {
+        // This logic extracts the path from either a ref or a query
+        const path: string =
+          memoizedTargetRefOrQuery.type === 'collection'
+            ? (memoizedTargetRefOrQuery as CollectionReference).path
+            : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString()
+
+        const contextualError = new FirestorePermissionError({
+          operation: 'list',
+          path,
+        })
+
+        setError(contextualError)
+        setData(null)
+        setIsLoading(false)
+
+        // trigger global error propagation
+        errorEmitter.emit('permission-error', contextualError);
       }
     );
 
     return () => unsubscribe();
   }, [memoizedTargetRefOrQuery]); // Re-run if the target query/reference changes.
-  
+  if(memoizedTargetRefOrQuery && !memoizedTargetRefOrQuery.__memo) {
+    throw new Error(memoizedTargetRefOrQuery + ' was not properly memoized using useMemoFirebase');
+  }
   return { data, isLoading, error };
 }
